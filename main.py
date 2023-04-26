980c18fa728ba17386a8ebec086de80dea4b770f
Jonathan Nesmith <nesmith.jonathan123@gmail.com>
2023-04-25 20:12:37 -0500

    add in income class

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ class Bill:
     def __init__(self, name='Bill', cost=0):
         self.name = name
         self.cost = cost
-        
+
+class Income:
+    def __init__(self, revenue,):
+        self.revenue = revenue
