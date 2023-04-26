7e04f13adb2c48fd35d5dabec714fc0f290df988
Eddie <Eddie943@outlook.com>
2023-04-25 20:33:20 -0500

    added in a new file

--- Work.py
+++ Work.py
@@ -1,1 +1,10 @@
+import main.py
 
+bills = []
+
+print("Add in the name of your bill followed by the cost, when done enter Q\n")
+endloop = 'H'
+name = ""
+cost = 0
+while(endloop != 'Q'):
+    bills.append(main.Bill(name, cost))
