7f8221301eaaeb73f7d1d51db301e5b2715f0630
Jonathan Nesmith <nesmith.jonathan123@gmail.com>
2023-04-25 20:20:04 -0500

    fixed income

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ class Bill:
     def __init__(self, name='Bill', cost=0):
         self.name = name
         self.cost = cost
-    
+class Income:    
     def __init__(self, revenue):
         self.revenue = revenue
-        
